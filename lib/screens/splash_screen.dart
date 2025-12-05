e60d043fb180a4b184bda0c2ed3b95b852fb9316
nilisha <nilishakhadgi29@gmail.com>
2025-12-05 10:24:31 +0545

    Add splash screen UI

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:tutorix/screens/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Auto navigate to onboarding after 2 seconds
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFCEEDD2),
+      body: Center(
+        child: Image.asset(
+          'assets/images/splashscreenlogo.png',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
+
