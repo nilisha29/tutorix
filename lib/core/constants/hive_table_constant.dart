a394baeedf3a5ba1fa85b667ed639a26221c68ac
nilisha <nilishakhadgi29@gmail.com>
2026-03-08 00:29:13 +0545

    update Hive table constants

--- lib/core/constants/hive_table_constant.dart
+++ lib/core/constants/hive_table_constant.dart
@@ -15,4 +15,12 @@ class HiveTableConstant {
   static const String sessionBox = "session_box";
   static const String currentAuthKey = "current_auth_id";
 
+  // Generic cache box for offline page data
+  static const String cacheBox = "cache_box";
+
+  // Cache keys
+  static const String tutorsCacheKey = "cache_tutors";
+  static const String categoriesCacheKey = "cache_categories";
+  static const String bookingsCacheKey = "cache_bookings";
+
 }
